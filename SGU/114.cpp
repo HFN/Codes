5b9ff325c6b6f900c5d958a53387191c5ea42293
HFN <ahaghani75@gmail.com>
2013-09-04 19:05:45 +0430

    Create 114.cpp

---
+++ SGU/114.cpp
@@ -0,0 +1,37 @@
+// SGU 114 Telecasting station
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <set>
+#include <map>
+#define ll long long
+#define x first
+#define y second
+using namespace std;
+const ll LONGINF = LLONG_MAX;
+const int INF = INT_MAX;
+const int MOD = 1000000007;
+int main()
+{
+	ios::sync_with_stdio(false);
+	float ans = 0;
+    int n, x, p, po = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> p;
+        ans += x * p;
+        po += p;
+    }
+    ans /= po;
+    cout << ans;
+	return 0;
+}
