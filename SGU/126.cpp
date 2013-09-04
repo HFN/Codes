e501a3605e4287016de5d9a1de3025776d5ba2e1
HFN <ahaghani75@gmail.com>
2013-09-04 19:14:26 +0430

    Update 126.cpp

--- SGU/126.cpp
+++ SGU/126.cpp
@@ -21,28 +21,28 @@ const int INF = 1000000;
 const int MOD = 1000000007;
 int main()
 {
-	  ios::sync_with_stdio(false);
-	  int a, b, c = 0;
+    ios::sync_with_stdio(false);
+    int a, b, c = 0;
     cin >> a >> b;
     while ((a != 0) && (b != 0))
     {
-		c++;
-		if (a > b)
-		{
-			a -= b;
-			b += b;
-		}
-		else     
-		{
-			b -= a;
-			a += a;
-		}
-		if (c == INF)
-			break;
+	c++;
+	if (a > b)
+	{
+	    a -= b;
+	    b += b;
+	}
+	else     
+	{
+	    b -= a;
+	    a += a;
+	}
+	if (c == INF)
+	    break;
     }
     if (c != INF)
     	cout << c;
     else
         cout << -1;
-	  return 0;
+    return 0;
 }
