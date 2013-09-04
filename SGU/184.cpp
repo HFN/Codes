b5a409e570419c9622eafbe1f5be3ec1d1ed72c5
HFN <ahaghani75@gmail.com>
2013-09-04 19:36:50 +0430

    Create 184.cpp

---
+++ SGU/184.cpp
@@ -0,0 +1,34 @@
+// SGU 184 Patties
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <set>
+#include <map>
+#define ll long long
+#define x first
+#define y second
+using namespace std;
+const ll LONGINF = LLONG_MAX;
+const int INF = INT_MAX;
+const int MOD = 1000000007;
+int main()
+{
+    int p, m, c, k, r, v, n = 10001;
+    cin >> p >> m >> c >> k >> r >> v;
+    if (p / k < n)
+       n = p / k;
+    if (m / r < n)
+       n = m / r;
+    if (c / v < n)
+       n = c / v;
+    cout << n;
+    return 0;
+}
